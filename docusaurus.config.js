536a9092994edded08c6e72d0e6ce8b61f276b91
JohnMark13 <mark@soonami.io>
2022-12-19 10:49:20 +0000

    Update favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,10 +35,7 @@ const config = {
         docs: {
           routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/foundance/foundance-docs",
         },
         blog: false,
         theme: {
